ea31801090e32dca98746d5ccc238f9daab7a397
Minh-Quan DAO <daominhquan9909@gmail.com>
2019-05-27 18:40:11 +0200

    Add image flipping

--- s1p10_training_utils.py
+++ s1p10_training_utils.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 import cv2 as cv
+from scipy.stats import bernoulli
 
 
 def save_lstm(lstm_obj, time_str, weight_dir):
@@ -25,15 +26,46 @@ def one_hot_encode(angle_id, num_class):
     return y
 
 
+def find_angle_id(angle, bin_edges):
+    """
+    Find id for idx-th row of center_cam_df w.r.t this row's steering angle
+    
+    Input:
+        idx (scalar): row index of center_cam_df 
+        df (pd.Dataframe): dataframe  
+        bin_edges (np.array): array of bin edges 
+    
+    Output:
+        ID of this angle
+    """
+    angle_ID = -1
+    i = 0
+    flag_found_bin = False
+    while i < len(bin_edges) - 1 and not flag_found_bin:
+        if angle >= bin_edges[i] and angle < bin_edges[i + 1]:
+            flag_found_bin = True
+            angle_ID = i  
+        else:
+            i += 1
+    
+    if not flag_found_bin:
+        # not found any bin contains this steering angle --> equal to the right edge of the last bin
+        angle_ID = i - 1
+    
+    return angle_ID
+
+
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, 
                  dataset_csv, data_root_dir, 
                  img_shape, 
                  num_class, 
-                 num_prediction, 
+                 num_prediction,
+                 bins_edge,
                  batch_size=30, 
                  shuffle=True, 
-                 lstm_dim_hidden_states=None):
+                 lstm_dim_hidden_states=None,
+                 flip_prob=0.5):
         """
         Input:
             dataset_csv (string): path to csv contains dataset
@@ -41,8 +73,11 @@ class DataGenerator(keras.utils.Sequence):
             img_shape (tuple): (img_height, img_width, img_channel)
             num_class (int): number of classes of angle
             num_prediction (int): number of angles will be predicted 
+            bins_edge (list): list of outter edge of every bin
             batch_size (int): size of a training batch
             shuffle (bool): shuffle dataset after 1 epoch
+            lstm_dim_hidden_states (int): dimension of hidden states of LSTM cell
+            flip_prob (float): probability of flipping image & subsequece steering angles
         """
         self.df = pd.read_csv(dataset_csv)
         self.data_root_dir = data_root_dir
@@ -60,6 +95,8 @@ class DataGenerator(keras.utils.Sequence):
         # for training a classifier 
         self.num_prediction = num_prediction
         self.num_class = num_class
+        self.flip_prob = flip_prob
+        self.bins_edge = bins_edge
         
         # for LSTM
         self.lstm_dim_hidden_states = lstm_dim_hidden_states
@@ -87,6 +124,9 @@ class DataGenerator(keras.utils.Sequence):
         X = np.zeros((self.batch_size, ) + self.img_shape)
         y = [np.zeros((self.batch_size, self.num_class)) for i in range(self.num_prediction)]
         
+        # decide when to flip the image
+        flip = bernoulli.rvs(self.flip_prob, size=self.batch_size) == 1
+        
         # Iterate through each idx in training batch
         for i, idx in enumerate(list_indexes): 
             # get image file name
@@ -103,13 +143,24 @@ class DataGenerator(keras.utils.Sequence):
             if len(img.shape) == 2:
                 img = img.reshape((self.img_shape))
             
+            # flip the image if any
+            if flip[i]:
+                img = np.fliplr(img)
+            
             # store image to X
             X[i, :, :, :] = img
             
-            # store angle to y
-            angle_id_list = self.df.iloc[idx].angle_id[1: -1].split(", ")
-            for j, angle_id in enumerate(angle_id_list):
-                y[j][i, :] = one_hot_encode(int(angle_id), self.num_class)
+            # create y
+            angle_val_list = self.df.iloc[idx].angle_val[1: -1].split(", ")
+            for j, angle_val in enumerate(angle_val_list):
+                if flip[i]:
+                    # flip image -> flip angle
+                    angle_id = find_angle_id(-float(angle_val), self.bins_edge)
+                else:
+                    # No flip
+                    angle_id = find_angle_id(float(angle_val), self.bins_edge)
+                # one-hot encode angle_id & store result to y
+                y[j][i, :] = one_hot_encode(angle_id, self.num_class)
         
         # create additional input for LSTM layer
         if self.lstm_dim_hidden_states:
